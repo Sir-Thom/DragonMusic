cd0c9076c56f47dacd68cdd764e31fbcd1a103be
Thomas-Toulouse-Cegep <2134063@cegepba.qc.ca>
2023-02-08 14:55:13 -0500

    layout

--- src/components/body.js
+++ src/components/body.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import { Container, Row,Card } from 'react-bootstrap';
+import { Container, Row,Card,Stack } from 'react-bootstrap';
 import './body.css'
+
 
 function Body(){
   return(
-  <div className='body '>
-  <Container fluid >
-   <Row className=" position-fixed justify-content">
-    
-      <Card  className='col-md-4 g-2 text-dark gx-2' >
+  <div  className='body '>
+   <Stack direction='horizontal' gap={3}>
+   
+    <Card  className='col-md-4 g-2 text-dark gx-2' >
         <Card.Body>
           <Card.Title>Card Title</Card.Title>
           <Card.Text>
@@ -24,9 +24,119 @@ function Body(){
           </Card.Text>
         </Card.Body>
       </Card>
-    </Row>
-  </Container>
+      
+      </Stack> 
+      <Stack  gap={3}>
+   
+   <Card  className='col-md-4 g-2 text-dark gx-2' >
+       <Card.Body>
+         <Card.Title>Card Title</Card.Title>
+         <Card.Text>
+           Some quick example text to build on the card title and make up the bulk of the card's content.
+         </Card.Text>
+       </Card.Body>
+     </Card>
+     <Card className='text-dark g-2' style={{  width: '18rem' }}>
+       <Card.Body>
+         <Card.Title>Card Title</Card.Title>
+         <Card.Text>
+           Some quick example text to build on the card title and make up the bulk of the card's content.
+         </Card.Text>
+       </Card.Body>
+     </Card>
+     
+     </Stack> 
+     <Stack  gap={3}>
+   
+   <Card  className='col-md-4 g-2 text-dark gx-2' >
+       <Card.Body>
+         <Card.Title>Card Title</Card.Title>
+         <Card.Text>
+           Some quick example text to build on the card title and make up the bulk of the card's content.
+         </Card.Text>
+       </Card.Body>
+     </Card>
+     <Card className='text-dark g-2' style={{  width: '18rem' }}>
+       <Card.Body>
+         <Card.Title>Card Title</Card.Title>
+         <Card.Text>
+           Some quick example text to build on the card title and make up the bulk of the card's content.
+         </Card.Text>
+       </Card.Body>
+     </Card>
+     
+     </Stack> 
+
+     <Stack  gap={3}>
+   
+   <Card  className='col-md-4 g-2 text-dark gx-2' >
+       <Card.Body>
+         <Card.Title>Card Title</Card.Title>
+         <Card.Text>
+           Some quick example text to build on the card title and make up the bulk of the card's content.
+         </Card.Text>
+       </Card.Body>
+     </Card>
+     <Card className='text-dark g-2' style={{  width: '18rem' }}>
+       <Card.Body>
+         <Card.Title>Card Title</Card.Title>
+         <Card.Text>
+           Some quick example text to build on the card title and make up the bulk of the card's content.
+         </Card.Text>
+       </Card.Body>
+     </Card>
+     
+     </Stack> 
+
+     <Stack  gap={3}>
+   
+   <Card  className='col-md-4 g-2 text-dark gx-2' >
+       <Card.Body>
+         <Card.Title>Card Title</Card.Title>
+         <Card.Text>
+           Some quick example text to build on the card title and make up the bulk of the card's content.
+         </Card.Text>
+       </Card.Body>
+     </Card>
+     <Card className='text-dark g-2' style={{  width: '18rem' }}>
+       <Card.Body>
+         <Card.Title>Card Title</Card.Title>
+         <Card.Text>
+           Some quick example text to build on the card title and make up the bulk of the card's content.
+         </Card.Text>
+       </Card.Body>
+     </Card>
+     <Stack  gap={3}>
+   
+   <Card  className='col-md-4 g-2 text-dark gx-2' >
+       <Card.Body>
+         <Card.Title>Card Title</Card.Title>
+         <Card.Text>
+           Some quick example text to build on the card title and make up the bulk of the card's content.
+         </Card.Text>
+       </Card.Body>
+     </Card>
+     <Card className='text-dark g-2' style={{  width: '18rem' }}>
+       <Card.Body>
+         <Card.Title>Card Title</Card.Title>
+         <Card.Text>
+           Some quick example text to build on the card title and make up the bulk of the card's content.
+         </Card.Text>
+       </Card.Body>
+     </Card>
+     
+     </Stack> 
+
+  
+     </Stack> 
+
+
+
   </div>
+
+
+
+
   );
 }
 
