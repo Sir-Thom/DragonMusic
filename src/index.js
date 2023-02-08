961a25c76f53bf55bb47fc60e32bbd2b9f15a6b9
Sirt-Thom <thomas_toulouse@yahoo.com>
2023-02-07 22:34:44 -0500

    layout

--- src/index.js
+++ src/index.js
@@ -1,25 +1,23 @@
 import React, { Fragment } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import NavbarApp  from './components/NavbarApp' 
+import Sidebar  from './components/Sidebar' 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'font-awesome/css/font-awesome.min.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCubes } from '@fortawesome/fontawesome-free-solid';
+import Body from './components/body'; 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-<Fragment>
-  <NavbarApp></NavbarApp>
-  <h3>Hello Alexis</h3>
-  {/* il y a plusieur méthode pour utiliser fontawesome 1: avec fontAwesomeIcon icon= */}
-  <FontAwesomeIcon icon=" fa fa-cubes"></FontAwesomeIcon> methode 1<br></br>
-  {/*1.2 ont peu aussi import une icon en particulier  */}
-  <FontAwesomeIcon icon={faCubes}></FontAwesomeIcon>methode 1.2
-   {/* 2: ont peu utiliser les className */}
-  <div><i className="fa fa-cubes ">methode 2</i></div>
+<div variant="dark">
 
-  </Fragment>
+  <Sidebar></Sidebar>
+  <Body></Body>
+  
+ 
+
+  </div>
 );
 
 // If you want to start measuring performance in your app, pass a function
