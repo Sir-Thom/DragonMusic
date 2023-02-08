fd886d999286dee124ef22d1f21c789bf3d6ec68
gabjacob04 <jedigabjacob@icloud.com>
2023-02-08 18:48:38 -0500

    NavBar

--- src/components/NavbarApp.js
+++ src/components/NavbarApp.js
@@ -2,40 +2,31 @@ import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
-import {Offcanvas, Form, Button } from 'react-bootstrap';
+//import NavDropdown from 'react-bootstrap/NavDropdown';
+import {Form, Button } from 'react-bootstrap';
 import './NavBarApp.css';
 
 function NavbarApp(){
     return(
         <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" className='d-flex text-dark flex-column flex-shrink-0 flex-fill p-3 a'>
       <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Brand href="#home">Connexion</Navbar.Brand>
+        <Navbar.Brand href="#home">Inscription</Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        {/*}<Navbar.Collapse id="responsive-navbar-nav">
+        <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="ml-auto">
-            <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-    </Navbar.Collapse>{*/}
+            <Nav.Link href="#deconnexion">Déconnexion</Nav.Link>
+        </Nav>
+        </Navbar.Collapse>
         <Nav>
             <Form className="d-flex mr-auto" expand="lg">
             <Form.Control
               type="search"
-              placeholder="Search"
+              placeholder="Recherche"
               className="me-2"
-              aria-label="Search"
+              aria-label="Recherche"
             />
-            <Button variant="outline-success">Search</Button>
+            <Button variant="outline-success">Rechercher</Button>
           </Form>
           </Nav>
       </Container>
