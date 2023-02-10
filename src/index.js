d6c7b586bffdb287615335aa667b49914fc738ea
Thomas-Toulouse-Cegep <2134063@cegepba.qc.ca>
2023-02-10 14:53:39 -0500

    refactor

--- src/index.js
+++ src/index.js
@@ -1,15 +1,20 @@
 import React, { Fragment } from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navigation from "./components/header/NavbarComp";
 import Body from "./components/body/body"
-
-
+//route aka page we will be using
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import SignIn from "./pages/SignIn";
 
 //import ButtonAppBar from './components/NavbarComp';
 const root = ReactDOM.createRoot(document.getElementById('root'));
+// pour savoir comment faire les routes https://www.w3schools.com/react/react_router.asp
 root.render(
+    
 <Fragment>
  <Navigation></Navigation>
 </Fragment>
