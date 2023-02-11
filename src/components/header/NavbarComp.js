2df1cc716057ae2c7fa0b8aee8e45bca47ba2705
Thomas-Toulouse-Cegep <102597615+Thomas-Toulouse-Cegep@users.noreply.github.com>
2023-02-10 21:17:19 -0500

    add animation

--- src/components/header/NavbarComp.js
+++ src/components/header/NavbarComp.js
@@ -3,18 +3,17 @@ import "./NavbarComp.css";
 import logo from "../../asset/logo.svg";
 import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
 import SearchBar from "../elements/SearchBar";
-
 function Navigation() {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <nav className="flex items-end shadow-md shadow-cod-gray  bg-cod-gray-600  rounded-br-lg p-4 rounded-bl">
+    <nav className="flex font-Ubuntu items-end shadow-md shadow-cod-gray  bg-cod-gray-600  rounded-br-lg p-4 rounded-bl">
       <div className=" max-w-full container  mx-0 text-white">
         <div className="flex items-center ms-auto pr-4 justify-between">
           <div className="flex text-lg  items-center ">
             <img className="h-[6vh] w-[6vw]" src={logo} alt="Logo" />
             <a href="/#"
-              className="font-bold  text-xl items-center no-underline hover:bg-white hover:text-gray-50 "
+              className="font-bold  text-xl items-center no-underline hover:text-gray-50 "
             >
               Dragon Music
             </a>
@@ -32,13 +31,13 @@ function Navigation() {
             >
               {isOpen ? (
                 <AiOutlineClose
-                  className="items-center menu-icon"
+                  className="items-center transition ease-in hover:scale-110 duration-300 menu-icon"
                   fontSize={25}
                   scale={25}
                 />
               ) : (
                 <AiOutlineMenu
-                  className="items-center menu-icon"
+                  className="transition items-center ease-in hover:scale-110 duration-300 menu-icon"
                   fontSize={25}
                   scale={25}
                 />
@@ -48,26 +47,27 @@ function Navigation() {
         </div>
         {isOpen && (
           <div
-            id="collapseExample"
-            className="collaspe bg-cod-gray-600 py-4  transition-all delay-100 duration-700  ease-linear"
+          className={`${
+            isOpen ? 'block  ' : 'hidden'
+          }  transform-gpu duration-500 ease-in-out transition-transform bg-cod-gray-600 py-4`}
           >
-            <div className="block font-semibold p-2 text-lg text-white hover:hover:bg-cod-gray-100">
-              <button className="block px-4 py-2 text-white hover:hover:bg-cod-gray-100">
+            <div className="block rounded-lg font-semibold p-2 text-lg text-white hover:hover:bg-cod-gray-100">
+              <button className=" px-4 py-2 text-white hover:hover:bg-cod-gray-100">
                 Accueil
               </button>
             </div>
-            <div className="block font-semibold p-2 text-lg text-white hover:hover:bg-cod-gray-100">
-              <button className="block px-4 py-2 text-white hover:hover:bg-cod-gray-100">
+            <div className="block transition  ease-in-out   duration-500 rounded-lg font-semibold p-2 text-lg text-white hover:hover:bg-cod-gray-100">
+              <button className=" px-4 py-2 text-white ">
                 bibi
               </button>
             </div>
-            <div className="block font-semibold p-2 text-lg text-white hover:hover:bg-cod-gray-100">
-              <button className="block px-4 py-2 text-white hover:hover:bg-cod-gray-100">
+            <div className="block rounded-lg transition  ease-in-out duration-500 font-semibold p-2 text-lg text-white hover:hover:bg-cod-gray-100">
+              <button className=" px-4 py-2 text-white">
                 Connexion
               </button>
             </div>
-            <div className="block font-semibold p-2 text-lg text-white hover:hover:bg-cod-gray-100">
-              <button className="block px-4 py-2 text-white hover:hover:bg-cod-gray-100">
+            <div className="block rounded-lg transition  ease-in-out  duration-500 font-semibold p-2 text-lg text-white hover:hover:bg-cod-gray-100">
+              <button className=" px-4 py-2 text-white">
                 Inscription
               </button>
             </div>
