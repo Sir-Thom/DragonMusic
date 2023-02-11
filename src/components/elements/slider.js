233641c254ffa7249879ec700a5213d4cd0cafe8
Thomas-Toulouse-Cegep <102597615+Thomas-Toulouse-Cegep@users.noreply.github.com>
2023-02-10 23:36:26 -0500

    add cardmusic

--- src/components/elements/slider.js
+++ src/components/elements/slider.js
@@ -1,13 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import  "./Searchbar.css";
+const Slider = () => {
+  const [value, setValue] = useState(50);
 
+  const handleChange = (event) => {
+    setValue(event.target.value);
+  };
 
+  return (
+    <div className="flex  mb-2 text-sm font-medium text-gray-900 dark:text-white items-center mt-10">
+      <input
+        type="range"
+        className="w-full h-2 bg-gray-200 rounded-lg appearance-none cursor-pointer dark:bg-gray-700"
+        value={value}
+        onChange={handleChange}
+        style={{
+          background: `linear-gradient(to right, #3182ce 0%, #3182ce ${value}%, #fff ${value}%, #fff 100%)`,
+          backgroundSize: `${value}% 100%`,
+        }}
+      />
+      <div className="ml-3 font-medium">{value}</div>
+    </div>
+  );
+};
 
-export default function Slider(props){
-    return(
-        <><label for="default-range" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Default range</label><input id="default-range" type="range" value="50" className="w-full h-2 bg-gray-200 rounded-lg appearance-none cursor-pointer dark:bg-gray-700">
-        </input></>
-
-
-    )
-}
+export default Slider;
