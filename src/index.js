aba5921cad64b2478674c47f35c388c184df70e8
gabjacob04 <jedigabjacob@icloud.com>
2023-02-12 23:47:32 -0500

    Oublier de remettre index comme avant

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React, { Fragment } from 'react';
 import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter, Routes, Route, useNavigate } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navigation from "./components/header/NavbarComp";
 import Body from './components/body/body';
-import { Connexion, Inscription } from './pages/connexionInscription';
+import { Connexion } from './pages/connexionInscription';
 import MusicBar from './components/elements/musicBar'
 //route aka page we will be using
 /*import Home from "./pages/Home";
@@ -17,7 +17,13 @@ import SignIn from "./pages/SignIn";*/
 const root = ReactDOM.createRoot(document.getElementById('root'));
 // pour savoir comment faire les routes https://www.w3schools.com/react/react_router.asp
 root.render(
-    <Inscription/>
+    <React.StrictMode>
+<Fragment>
+ <Navigation></Navigation>
+<Body></Body>
+<MusicBar></MusicBar>
+</Fragment>
+</React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
