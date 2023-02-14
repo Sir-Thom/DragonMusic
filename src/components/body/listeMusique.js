cdfc283ad8a877159ebe263102c01a6b94d24791
Alexis Lavertu <2134071@cegepba.qc.ca>
2023-02-14 12:00:19 -0500

    image qui marche dans asset

--- src/components/body/listeMusique.js
+++ src/components/body/listeMusique.js
@@ -3,20 +3,19 @@ import './listeMusique.css';
 import BouttonJouerMusique from '../elements/boutonJouerMusique';
 import {AiOutlinePauseCircle} from "react-icons/ai";
 import { ImPlay2 } from "react-icons/im";
-import goofy_dragon from "../../asset/goofy_dragon.png";
-const musiques = require('../data/musique.json');
-
+const musiques = require("../data/musique.json");
 export default function ListeDeMusique(){
     return (
       <div className="flex gap-2 flex-shrink-0 ">
         {musiques.map((musique, index) => (
           <div key={index} className=" flex-row w-25  bg-cod-gray-600   flex rounded overflow-hidden ">
             <img
-              src={goofy_dragon}//"../../asset/goofy_dragon.png"
+              src={musique.image}//"../../asset/goofy_dragon.png"
               width={150}
               alt={musique.nomMusique}
               className="w-partial"
             />
+           
             <div className="px-6 py-4">
               <div className="font-bold text-xl mb-2">{musique.nomMusique}</div>
               <p className="text-white text-base">{musique.auteur}</p>
