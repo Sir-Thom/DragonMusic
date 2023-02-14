443d320f809b1ad1ed5f20065b10f638c9939d80
gabjacob04 <jedigabjacob@icloud.com>
2023-02-14 09:16:21 -0500

    page d'erreur

--- src/pages/pageError.js
+++ src/pages/pageError.js
@@ -1,5 +1,6 @@
 
 import { useRouteError  } from "react-router-dom";
+import {Link} from "react-router-dom";
 
 export default function ErrorPage() {
   const error = useRouteError();
@@ -13,6 +14,13 @@ export default function ErrorPage() {
       <p>
         <i  className="text-xl font-medium text-white">{error.statusText || error.message}</i>
       </p>
+      <br></br>
+      <button className=" inline-block bg-indigo-600 ms-auto px-6 transition ease-in hover:scale-200 py-2 border-2 border-violet-600 text-gray-100 font-medium text-xs leading-tight uppercase rounded-full hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0 " 
+                                    type="button" id="button-addon3">
+            <Link to="/" className="relative px-4 py-2 text-white ">
+            Retourner à la page d'accueil
+          </Link>
+        </button>
     </div>
     </div>
   );
