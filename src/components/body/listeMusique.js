71224a3b46311689c088023a4aee5eb1c4cd623b
Thomas-Toulouse-Cegep <102597615+Thomas-Toulouse-Cegep@users.noreply.github.com>
2023-02-14 11:03:34 -0500

    jdaksl# Please enter the commit message for your changes. Lines starting

--- src/components/body/listeMusique.js
+++ src/components/body/listeMusique.js
@@ -1,27 +1,22 @@
 import React, { Fragment ,useState} from 'react';
 import './listeMusique.css';
 import BouttonJouerMusique from '../elements/boutonJouerMusique';
-import {AiOutlinePauseCircle} from "react-icons/ai";
-import { ImPlay2 } from "react-icons/im";
-import goofy_dragon from "../../asset/goofy_dragon.png";
+
 const musiques = require('../data/musique.json');
 
 export default function ListeDeMusique(){
     return (
-      <div className="grid grid-cols-3 grid-rows-2  gap-2  ">
-        {music.map((musique, index) => (
-          <div key={index} className="   bg-cod-gray-600   flex rounded overflow-hidden ">
       <div className="flex gap-2 flex-shrink-0 ">
         {musiques.map((musique, index) => (
           <div key={index} className=" flex-row w-25  bg-cod-gray-600   flex rounded overflow-hidden ">
             <img
-              src={goofy_dragon}//"../../asset/goofy_dragon.png"
+              src={require(musique.image)}//"../../asset/goofy_dragon.png"
               width={150}
               alt={musique.nomMusique}
-              className=" min-w-0 flex flex-shrink-0"
+              className="w-partial"
             />
             <div className="px-6 py-4">
-              <div className=" flex-shrink-0font-bold text-xl mb-2">{musique.nomMusique}</div>
+              <div className="font-bold text-xl mb-2">{musique.nomMusique}</div>
               <p className="text-white text-base">{musique.auteur}</p>
               <p className="text-white text-base">{musique.duree}</p>
             </div>
@@ -32,5 +27,3 @@ export default function ListeDeMusique(){
         ))}
       </div>
     );}
-
-
