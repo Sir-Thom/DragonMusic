155231f39b8e377a5263b0ff92839797ccfed5d5
Thomas-Toulouse-Cegep <102597615+Thomas-Toulouse-Cegep@users.noreply.github.com>
2023-02-15 22:12:07 -0500

    new animations

--- src/components/elements/musicBar.js
+++ src/components/elements/musicBar.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
-import {ImPlay2,ImNext ,ImPrevious} from "react-icons/im"
-
+import {BsPlayCircleFill,BsPauseCircleFill,BsSkipStartCircleFill ,BsSkipEndCircleFill} from "react-icons/bs"
+const musiques = require('../data/musique.json');
 
 function MusicBar(){
   const [currentTime, setCurrentTime] = useState(0);
@@ -11,7 +11,7 @@ date.setSeconds(currentTime);
 const timeString = date.toISOString().substr(11, 8);
 console.log(timeString)
   return (
-    <nav className="fixed bottom-0 w-full rounded bg-gray-800 p-2">
+    <nav className="fixed   right-0 bottom-0 w-full rounded bg-gray-800">
     <div className="relative block h-1 w-full ">
       <input
         type="range"
@@ -19,21 +19,21 @@ console.log(timeString)
         max={duration}
         value={currentTime}
         onChange={(e) => setCurrentTime(e.target.value)}
-        className=" inline-block w-full bg-gray-700"
+        className=" inline-flex w-full bg-gray-700"
       />
-      <p className="text-white text-sm">{timeString}</p>
-    </div>
+ <p className="text-white text-sm  ml-2">{timeString}</p>    
+ </div>
   
     <div className="flex justify-center items-center text-xs font-semibold text-gray-500 px-4 py-3">
       <div className="space-x-3 p-2">
         <button onClick={() => { alert("previous") }} className="focus:outline-none">
-          <ImPrevious className="relative text-white" size={30}></ImPrevious>
+          <BsSkipStartCircleFill className=" relative text-violet-600 hover:scale-110 duration-300 transform-gpu transition ease-in-out delay-150" size={30}></BsSkipStartCircleFill>
         </button>
-        <button onClick={() => { alert("play") }} className="focus:outline-none">
-          <ImPlay2 className="relative text-white" size={35}></ImPlay2>
+        <button onClick={() => { alert("play") }} className="snap-center items-center focus:outline-none">
+          <BsPlayCircleFill className="relative text-violet-600 hover:scale-110 duration-300 transform-gpu transition ease-in-out delay-150" size={35}></BsPlayCircleFill>
         </button>
         <button onClick={() => { alert("next") }} className="focus:outline-none">
-          <ImNext size={30} className="relative focus:outline-none text-white"></ImNext>
+          <BsSkipEndCircleFill size={30} className="relative hover:scale-110 duration-300 focus:outline-none text-violet-600"></BsSkipEndCircleFill>
         </button>
       </div>
     </div>
