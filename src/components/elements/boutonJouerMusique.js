009ec29c845a110f30fb101ab80d3f5b073bb815
Thomas-Toulouse-Cegep <102597615+Thomas-Toulouse-Cegep@users.noreply.github.com>
2023-02-17 10:13:23 -0500

    restyle

--- src/components/elements/boutonJouerMusique.js
+++ src/components/elements/boutonJouerMusique.js
@@ -12,7 +12,7 @@ function BouttonJouerMusique() {
   return (
     <button
       onClick={Play}
-      className="bg-violet-500 hover:bg-violet-700 text-white font-bold py-2 px-4 rounded-full"
+      className="  bg-violet-500 hover:bg-violet-600 text-white font-bold py-2 px-4 rounded-full hover:scale-110 duration-300 transform-gpu transition ease-in-out delay-150"
     >
       {selectedIcon === 1 ? <ImPlay2 /> : <ImPause />}
     </button>
