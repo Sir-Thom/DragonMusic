b38003fb8d9bd33a3029d721299c6c135d2f8220
gabjacob04 <jedigabjacob@icloud.com>
2023-02-21 09:28:30 -0500

    music

--- src/components/body/body.js
+++ src/components/body/body.js
@@ -15,7 +15,7 @@ const StockListWithSearch = SearchBar(StockMusique, (item, searchTerm) => {
 
 function Body(){
   return(
-<div className=" overflow-y-scroll gradiant">
+<div className="overflow-y-scroll gradiant">
       <div className=" mb-8">
         <StockListWithSearch data={stocks}/>
       </div>
