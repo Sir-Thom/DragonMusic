e42f988ca7f3b5776743dba418d3fce3b2bd707c
Alexis Lavertu <2134071@cegepba.qc.ca>
2023-02-21 11:45:30 -0500

    get id musique

--- src/components/elements/boutonJouerMusique.js
+++ src/components/elements/boutonJouerMusique.js
@@ -1,14 +1,17 @@
 import React, { useState } from "react";
 import { ImPlay2, ImPause } from "react-icons/im";
-
+import Music from "./musicBar";
 function BouttonJouerMusique(props) {
   const [CasePlayStop, setSelectedIcon] = useState(1);
 
-  function Play(e) {
-    e.preventDefault();
+  function Play(e,idMusique) {
+   e.preventDefault();
     setSelectedIcon(CasePlayStop === 1 ? 2 : 1);
     if(CasePlayStop===1){
-      alert(e);
+      alert(idMusique);
+    
+      Music(idMusique);
+      //if(){}
     }else if(CasePlayStop===2){
       alert("stop musique"); 
     }
@@ -16,7 +19,7 @@ function BouttonJouerMusique(props) {
 
   return (
     <button
-      onClick={(e) => Play(e)}
+      onClick={(e) => Play(e,props.idMusique)}
       className="  bg-violet-500 hover:bg-violet-600 text-white font-bold py-2 px-4 rounded-full hover:scale-110 duration-300 transform-gpu transition ease-in-out delay-150"
     >
       {CasePlayStop === 1 ? <ImPlay2 /> : <ImPause />}
