367253e8dd1900e13b09d32007a0c70a4e9e5e32
Thomas-Toulouse-Cegep <2134063@cegepba.qc.ca>
2023-03-28 09:22:05 -0400

    Update Program.cs

--- API-AGT-Web/Program.cs
+++ API-AGT-Web/Program.cs
@@ -3,7 +3,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
