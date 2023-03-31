a64cba6fb653753b23781137bdc28c2d0ab161a6
Alexis Lavertu <2134071@cegepba.qc.ca>
2023-03-31 09:56:06 -0400

    chose qui marche presque

---
+++ API-AGT-Web/Controllers/MusicController.cs
@@ -0,0 +1,23 @@
+﻿using API_AGT_Web.Music.Data;
+using Microsoft.AspNetCore.Mvc;
+using API_AGT_Web.Music;
+
+namespace API_AGT_Web.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MusicController : ControllerBase
+    {
+        private readonly IMusic musicRepository;
+        public MusicController(IConfiguration configuration)
+        {
+         musicRepository = new MusicLiteDbRepository(configuration["LiteDbFilePath"]);
+         
+        }
+        [HttpGet]
+        public IEnumerable<Music.Music> Get()
+        {
+            return musicRepository.GetMusics();
+        }
+    }
+}
---
+++ API-AGT-Web/Musics/Music.cs
@@ -0,0 +1,12 @@
+﻿namespace API_AGT_Web.Music
+{
+    public class Music
+    {
+        public string NomMusique { get; set; }
+        public int Duree { get; set; }
+        public string Auteur { get; set; }
+        public string Image { get; set; }
+        
+      
+    }
+}
