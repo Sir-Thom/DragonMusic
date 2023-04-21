0342ea12a3603dd33fba3cb92940edec25d95c3c
gabjacob04 <jedigabjacob@icloud.com>
2023-04-21 15:06:07 -0400

    Save

---
+++ API-AGT/API-AGT-Web/Musics/Data/IMusicLiteDbRepository.cs
@@ -0,0 +1,13 @@
+﻿using API_AGT_Web.Users;
+
+namespace API_AGT_Web.Music.Data
+{
+    public interface IMusicLiteDbRepository
+    {
+        public IEnumerable<Music> GetMusics();
+
+        Music GetMusicById(int idMusic); 
+
+        void AddMusic(Music music);
+    }
+}
---
+++ API-AGT/API-AGT-Web/Musics/Music.cs
@@ -0,0 +1,11 @@
+﻿namespace API_AGT_Web.Music
+{
+    public class Music
+    {
+        public int Id { get; set; }
+        public string NomMusique { get; set; } = "";
+        public int Duree { get; set; } = 0;
+        public string Auteur { get; set; } = "";
+        public string Image { get; set; } = "";
+    }
+}
