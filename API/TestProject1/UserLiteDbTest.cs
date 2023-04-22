05dbf4a8ff1ab7cdc0bf0232fa37ce1415b6e8d5
Thomas-Toulouse-Cegep <2134063@cegepba.qc.ca>
2023-04-21 22:36:23 -0400

    2e test qui marche les boys

--- API/TestProject1/UserLiteDbTest.cs
+++ API/TestProject1/UserLiteDbTest.cs
@@ -1,23 +1,99 @@
 using API_AGT_Web.Users.Data;
+using LiteDB;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace API_AGT_Web.Tests
 {
+    public interface ILiteCollection<T>
+    {
+        IQueryable<T> FindAll();
+    }
+
+    public interface ILiteDatabaseWrapper
+    {
+        ILiteCollection<T> GetCollection<T>();
+    }
+
+    public class LiteCollectionWrapper<T> : ILiteCollection<T>
+    {
+        private LiteCollection<T> _collection;
+
+        public LiteCollectionWrapper(LiteCollection<T> collection)
+        {
+            _collection = collection;
+        }
+
+        public IQueryable<T> FindAll()
+        {
+            return _collection.FindAll().AsQueryable();
+        }
+    }
+
+    public class LiteDatabaseWrapper : ILiteDatabaseWrapper
+    {
+        private LiteDatabase _database;
+
+        public LiteDatabaseWrapper(string connectionString)
+        {
+            _database = new LiteDatabase(connectionString);
+        }
+
+        public ILiteCollection<T> GetCollection<T>()
+        {
+            return new LiteCollectionWrapper<T>((LiteCollection<T>)_database.GetCollection<T>());
+        }
+    }
+
     [TestClass]
     public class UserLiteDbTest
     {
-        [TestMethod]
-        public void GetUsers_ReturnsThreeUsers()
+        [TestClass]
+        public class UsersLiteDbRepositoryTests
         {
-            // Arrange
-            var userRepository = new UserInMemoryRepository();
+            private Mock<ILiteDatabaseWrapper> mockDatabase;
+            private UsersLiteDbRepository repository;
 
-            // Act
-            var users = userRepository.GetUsers();
+            [TestMethod]
+            public void GetUsers_ReturnsUsersFromDatabase()
+            {
+                // Arrange
+                var collectionMock = new Mock<ILiteCollection<UserEntity>>();
+                var users = new List<UserEntity>
+    {
+        new UserEntity { Name = "John Doe", Email = "johndoe@example.com", PasswordHash = "password" },
+        new UserEntity { Name = "Jane Smith", Email = "janesmith@example.com", PasswordHash = "password123" },
+        new UserEntity { Name = "human1", Email = "human1@example.com", PasswordHash = "password" }
+    };
+                collectionMock.Setup(c => c.FindAll()).Returns(users.AsQueryable());
+                mockDatabase.Setup(db => db.GetCollection<UserEntity>()).Returns(collectionMock.Object);
 
-            // Assert
-            Assert.AreEqual(3, users.Count());
+                // Act
+                var result = repository.GetUserByUsername("human1");
+                result.Name = "human1";
+
+                // Assert
+                Assert.AreEqual("human1", result.Name);
+            }
+
+            [TestInitialize]
+            public void TestInitialize()
+            {
+                mockDatabase = new Mock<ILiteDatabaseWrapper>();
+                var connectionString = "Filename=..\\Music.db; Connection=Shared;";
+                var users = new List<UserEntity>
+                {
+                    new UserEntity { Name = "John Doe", Email = "johndoe@example.com", PasswordHash = "password" },
+                    new UserEntity { Name = "Jane Smith", Email = "janesmith@example.com", PasswordHash = "password123" }
+                };
+                var collectionMock = new Mock<ILiteCollection<UserEntity>>();
+                collectionMock.Setup(c => c.FindAll()).Returns(users.AsQueryable());
+                mockDatabase.Setup(db => db.GetCollection<UserEntity>()).Returns(collectionMock.Object);
+
+                repository = new UsersLiteDbRepository(connectionString);
+            }
         }
     }
 }
