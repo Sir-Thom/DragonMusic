cfd27f639894cf29c11f73c29eea07cd294d8315
Sir-Thom <102597615+Thomas-Toulouse-Cegep@users.noreply.github.com>
2023-04-23 22:16:17 -0400

    plus de bonne musique

--- src/components/elements/boutonJouerMusique.js
+++ src/components/elements/boutonJouerMusique.js
@@ -7,46 +7,42 @@ function BouttonJouerMusique(props) {
   const [CasePlayStop, setSelectedIcon] = useState(1);
   const [currentMusicId, setCurrentMusicId] = useState(MusicContext);
   const audioFiles = (e) => {
-    switch(e) {
+    switch (e) {
       case 1:
-        return "/asset/Hamburger Cheeseburger Big Mac Whopper (Full Version).mp3";
+        return "/asset/music/Hamburger Cheeseburger Big Mac Whopper (Full Version).mp3";
       case 2:
-        return "audio-file2.mp3";
+        return "/asset/music/Hank steel the real queer cowboy.mp3";
       case 3:
-        return "audio-file3.mp3";
+        return "/asset/music/Enormous Penis.mp3";
       default:
         return "/asset/Hamburger Cheeseburger Big Mac Whopper (Full Version).mp3";
     }
   };
 
-  const loadMusiqueChoisi = async () =>{
-    await fetch("https://localhost:7246/Music/"+props.idMusique, {
-      mode: 'cors',
-      method: 'GET'
+  const loadMusiqueChoisi = async () => {
+    await fetch("https://localhost:7246/Music/" + props.idMusique, {
+      mode: "cors",
+      method: "GET",
     })
-    .then((response) => 
-      response.json()
-    )
-    .then((music) => {
-      setCurrentMusicId(music.id);
-      console.log(music.id);
-    });
+      .then((response) => response.json())
+      .then((music) => {
+        setCurrentMusicId(music.id);
+        console.log(music.id);
+      });
   };
 
-  function Play(e, idMusique)  {
-    e.preventDefault(); 
+  function Play(e, idMusique) {
+    e.preventDefault();
     loadMusiqueChoisi();
-    
+
     setSelectedIcon(CasePlayStop === 1 ? 2 : 1);
     const audio = document.getElementById("audio" + idMusique);
-    
 
     //alert(props.idMusique);
     if (CasePlayStop === 1) {
       audio.play();
-     // alert( currentMusicId ); 
-    } 
-    else if (CasePlayStop === 2) {
+      // alert( currentMusicId );
+    } else if (CasePlayStop === 2) {
       audio.pause();
     }
   }
