01dc1a00a6cfd8db38baba1f158f5442ae142f40
Sir-Thom <2134063@cegepba.qc.ca>
2023-05-01 15:03:34 -0400

    bug fixes

--- API/API-AGT-Web/Musics/Data/MusicLiteDbRepository.cs
+++ API/API-AGT-Web/Musics/Data/MusicLiteDbRepository.cs
@@ -1,10 +1,12 @@
 ﻿using API_AGT_Web.Users.Data;
 using API_AGT_Web.Users;
 using LiteDB;
-using System.IO;
-using NAudio.Wave;
-using System.Reflection.Metadata.Ecma335;
 
+
+using System.Reflection.Metadata.Ecma335;
+using System;
+using System.IO;
+using Xabe.FFmpeg;
 namespace API_AGT_Web.Music.Data
 {
     public class MusicLiteDbRepository : IMusicLiteDbRepository
@@ -42,7 +44,7 @@ namespace API_AGT_Web.Music.Data
                 {
                     Id=1,
                     NomMusique = "RIP",
-                    Duree = setupDurationForMusicInMemory("..\\..\\public\\asset\\music\\Nintendo Wii - Mii Channel Theme.mp3"),
+                    Duree = setupDurationForMusicInMemory("../../public/asset/music/Nintendo Wii - Mii Channel Theme.mp3"),
                     Auteur = "Grim Reaper",
                     Image = "asset/died.gif",
                     MusicFile ="asset/music/Nintendo Wii - Mii Channel Theme.mp3"
@@ -51,24 +53,24 @@ namespace API_AGT_Web.Music.Data
                 {
                     Id=2,
                     NomMusique = "testation",
-                    Duree = setupDurationForMusicInMemory("..\\..\\public\\asset\\music\\Hank steel the real queer cowboy.mp3"),
+                    Duree = setupDurationForMusicInMemory("../../public/asset/music/Lynyrd Skynyrd-Free Bird (GUITAR SOLO).mp3"),
                     Auteur = "Jean-Marc",
                     Image = "asset/goofy_dragon.png",
-                    MusicFile ="asset/music/Hank steel the real queer cowboy.mp3"
+                    MusicFile ="asset/music/Lynyrd Skynyrd-Free Bird (GUITAR SOLO).mp3"
                 },
                 new Music(){
                     Id=3,
                     NomMusique = "test2",
-                    Duree = setupDurationForMusicInMemory("..\\..\\public\\asset\\music\\Hank steel the real queer cowboy.mp3"),
+                    Duree = setupDurationForMusicInMemory("../../public/asset/music/Lynyrd Skynyrd - Sweet Home Alabama (Audio).mp3"),
                     Auteur = "BABAJE",
                     Image = "asset/spag.png",
-                    MusicFile ="asset/music/Hank steel the real queer cowboy.mp3"
+                    MusicFile ="asset/music/Lynyrd Skynyrd - Sweet Home Alabama (Audio).mp3"
                 },
                 new Music()
                 {
                     Id=4,
                     NomMusique = "Sad song",
-                    Duree = setupDurationForMusicInMemory("..\\..\\public\\asset\\music\\Hank steel the real queer cowboy.mp3"),
+                    Duree = setupDurationForMusicInMemory("../../public/asset/music/Hank steel the real queer cowboy.mp3"),
                     Auteur = "Gabriel",
                     Image = "asset/Moai.png",
                     MusicFile ="asset/music/Hank steel the real queer cowboy.mp3"
@@ -76,70 +78,93 @@ namespace API_AGT_Web.Music.Data
                 new Music()
                 {
                     Id=5,
-                    NomMusique = "testation2",
-                    Duree = setupDurationForMusicInMemory("..\\..\\public\\asset\\music\\Hank steel the real queer cowboy.mp3"),
-                    Auteur = "Maxime",
+                    NomMusique = "Moyai Kart OST",
+                    Duree = setupDurationForMusicInMemory("../../public/asset/music/Coconut Mall - Mario Kart Wii OST.mp3"),
+                    Auteur = "Moyai",
                     Image = "asset/MoaiVoiture.png",
-                    MusicFile ="asset/music/Hank steel the real queer cowboy.mp3"
+                    MusicFile ="asset/music/Coconut Mall - Mario Kart Wii OST.mp3"
                 },
                 new Music()
                 {
                     Id=6,
                     NomMusique = "Fishium",
-                    Duree = setupDurationForMusicInMemory("..\\..\\public\\asset\\music\\Hank steel the real queer cowboy.mp3"),
-                    Auteur = "Maxance Gusse",
+                    Duree = setupDurationForMusicInMemory("../../public/asset/music/fish.mp3"),
+                    Auteur = "Le Fishe",
                     Image = "asset/fish.gif",
-                    MusicFile ="asset/music/Hank steel the real queer cowboy.mp3"
+                    MusicFile ="asset/music/fish.mp3"
                 },
                 new Music()
                 {
                     Id=7,
                     NomMusique = "we like fortnite",
-                    Duree = setupDurationForMusicInMemory("..\\..\\public\\asset\\music\\Hank steel the real queer cowboy.mp3"),
+                    Duree = setupDurationForMusicInMemory("../../public/asset/music/Rubber Band.mp3"),
                     Auteur = "FortiniteGamer",
                     Image = "asset/fortiniteSong.png",
-                    MusicFile ="asset/music/Hank steel the real queer cowboy.mp3"
+                    MusicFile ="asset/music/Rubber Band.mp3"
                 },
                 new Music()
                 {
                     Id=8,
-                    NomMusique = "we like fortnite2",
-                    Duree = setupDurationForMusicInMemory("..\\..\\public\\asset\\music\\Hank steel the real queer cowboy.mp3"),
-                    Auteur = "Generated Gusse Feet",
+                    NomMusique = "Smelly Feet",
+                    Duree = setupDurationForMusicInMemory("../../public/asset/music/Feet.mp3"),
+                    Auteur = "Feet",
                     Image = "asset/GoussePied.png",
-                    MusicFile ="asset/music/Hank steel the real queer cowboy.mp3"
+                    MusicFile ="asset/music/Feet.mp3"
                 },
                 new Music()
                 {
                     Id=9,
                     NomMusique = "I am dancing",
-                    Duree = setupDurationForMusicInMemory("..\\..\\public\\asset\\music\\Hank steel the real queer cowboy.mp3"),
+                    Duree = setupDurationForMusicInMemory("../../public/asset/music/Aaron Smith - Dancin - Krono Remix (Official Video) ft. Luvli.mp3"),
                     Auteur = "Green Dancing Guy",
                     Image = "asset/dance.webp",
-                    MusicFile ="asset/music/Hank steel the real queer cowboy.mp3"
+                    MusicFile ="asset/music/Aaron Smith - Dancin - Krono Remix (Official Video) ft. Luvli.mp3"
                 },
                 new Music()
                 {
                     Id=10,
                     NomMusique = "Moyai",
-                    Duree = setupDurationForMusicInMemory("..\\..\\public\\asset\\music\\bluegrass banjo - country banjo.mp3"),
+                    Duree = setupDurationForMusicInMemory("../../public/asset/music/bluegrass banjo - country banjo.mp3"),
                     Auteur = "Le Bolduc",
                     Image = "asset/moyai-dancing.gif",
                     MusicFile ="asset/music/bluegrass banjo - country banjo.mp3"
-                }
+                },
+                   new Music()
+                {
+                    Id=11,
+                    NomMusique = "Giga Chad",
+                    Duree = setupDurationForMusicInMemory("../../public/asset/music/gigachad.mp3"),
+                    Auteur = "Chad",
+                    Image = "asset/chad.jpeg",
+                    MusicFile ="asset/music/gigachad.mp3"
+                },
+                new Music()
+                {
+                    Id=12,
+                    NomMusique = "Hamburger",
+                    Duree = setupDurationForMusicInMemory("../../public/asset/music/Hamburger Cheeseburger Big Mac Whopper (Full Version).mp3"),
+                    Auteur = "Hamburger",
+                    Image = "asset/hamburger.gif",
+                    MusicFile ="asset/music/Hamburger Cheeseburger Big Mac Whopper (Full Version).mp3"
+                },
             };
             return musicList;
         }
 
+
         private int setupDurationForMusicInMemory(string path)
         {
-            var fileInfo = new FileInfo(path);
+            var music = Xabe.FFmpeg.FFmpeg.GetMediaInfo(path).Result;
+            var musicDuration = music.Duration.TotalSeconds;
+            var duration = Math.Floor(musicDuration);
+            return (int)duration;
+            /* var fileInfo = new FileInfo(path);
 
-            using (var audioFile = new AudioFileReader(path))
-            {
-                var duration = audioFile.TotalTime.TotalSeconds;
-                return (int)Math.Round(duration);
-            }
+                        using (var file = System.IO.File.Create(filePath))
+                        {
+                            var duration = (int)file.Properties.Duration.TotalSeconds;
+                            return duration;
+                        }*/
         }
 
 
