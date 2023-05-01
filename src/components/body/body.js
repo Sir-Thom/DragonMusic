523f8f3e790f4cad6f7203a51d0fc548a7cc4bf4
Sir-Thom <2134063@cegepba.qc.ca>
2023-05-01 15:56:32 -0400

    control de musci qui marche

--- src/components/body/body.js
+++ src/components/body/body.js
@@ -1,17 +1,171 @@
 import { React, useState, useEffect, useContext } from "react";
 
 import "./body.css";
-import MusicBar from "../elements/musicBar";
+//import MusicBar from "../elements/musicBar";
 import StockMusique from "../body/listeMusique";
 import SearchBar from "../elements/SearchBar";
-import play from "../elements/boutonJouerMusique";
+//import play from "../elements/boutonJouerMusique";
 import { MusicContext, MusicProvider } from "../elements/musicContext";
+import {
+  BsPlayCircleFill,
+  BsSkipStartCircleFill,
+  BsPauseCircleFill,
+  BsSkipEndCircleFill,
+} from "react-icons/bs";
+
 const stocks = require("../data/musique.json");
 
 const StockListWithSearch = SearchBar(StockMusique, (item, searchTerm) => {
   return item.nomMusique.toUpperCase().indexOf(searchTerm) >= 0;
 });
 
+// début partie musicbar
+
+function ConvertTime(timestamp) {
+  let minutes = Math.floor(timestamp / 60);
+  let seconds = timestamp - minutes * 60;
+  if (seconds < 10) {
+    seconds = "0" + seconds;
+  }
+  timestamp = minutes + ":" + seconds;
+  return timestamp;
+}
+
+function Play(e, idMusique) {
+  e.preventDefault();
+  const [CasePlayStop, setSelectedIcon] = useState(1);
+  setSelectedIcon(CasePlayStop === 1 ? 2 : 1);
+  const audio = document.getElementById(
+    "audio" + localStorage.getItem("idMusique")
+  );
+  if (CasePlayStop === 1) {
+    audio.play();
+  } else if (CasePlayStop === 2) {
+    audio.pause();
+  }
+}
+export function MusicBar(props) {
+  const [currentTime, setCurrentTime] = useState(0);
+  const [duration, setDuration] = useState(100);
+  const [changerButtonPlay, setChangerButtonPlay] = useState(true);
+  const [
+    currentMusicId,
+    setCurrentMusicId,
+    Musics,
+    setMusics,
+    musicTime,
+    setMusicTime,
+  ] = useContext(MusicContext);
+  const date = new Date(0);
+  date.setSeconds(currentTime);
+  const timeString = date.toISOString().substr(14, 5);
+  useEffect(() => {
+    const loadMusiqueChoisi = async () => {
+      await fetch("https://localhost:7246/Music/" + props.idMusique, {
+        mode: "cors",
+        method: "GET",
+      })
+        .then((response) => response.json())
+        .then((music) => {
+          setCurrentMusicId(music.id);
+          console.log(music.duree);
+          //setMusics(music.musi);
+          setMusicTime(music.duree);
+        });
+    };
+  });
+  console.log(musicTime);
+  function changerStateButtonPLay() {
+    if (changerButtonPlay === true) {
+      setChangerButtonPlay(false);
+    } else {
+      setChangerButtonPlay(true);
+    }
+  }
+
+  return (
+    <nav className="isolate h-20 absolute right-0 bottom-0 w-full rounded-tl-lg rounded-trt-lg  bg-cod-gray-700">
+      <div className=" block h-1 w-full ">
+        <input
+          type="range"
+          min={0}
+          max={localStorage.getItem("duration")}
+          value={currentTime}
+          onChange={(e) => setCurrentTime(musicTime)}
+          className="range range-xs range-primary h-1/4 mt-1 "
+        />
+        <p className="text-white text-sm  ml-2">{}</p>
+      </div>
+
+      <div className="flex justify-center  items-center text-xs font-semibold text-gray-500 px-4 py-3">
+        <div className="space-x-3 p-2">
+          <button
+            onClick={() => {
+              alert("previous");
+            }}
+            className="focus:outline-none"
+          >
+            {}
+            <BsSkipStartCircleFill
+              className=" relative text-violet-600 hover:scale-110 duration-300 transform-gpu transition ease-in-out "
+              size={30}
+            ></BsSkipStartCircleFill>
+          </button>
+          <button
+            id={"audio" + localStorage.getItem("idMusique")}
+            onClick={() => changerStateButtonPLay()}
+            className="snap-center items-center focus:outline-none"
+          >
+            {changerButtonPlay === false ? (
+              <BsPauseCircleFill
+                className="relative text-violet-600 hover:scale-110 duration-300 transform-gpu transition ease-in-out "
+                size={35}
+              ></BsPauseCircleFill>
+            ) : (
+              <BsPlayCircleFill
+                className="relative text-violet-600 hover:scale-110 duration-300 transform-gpu transition ease-in-out "
+                size={35}
+              ></BsPlayCircleFill>
+            )}
+          </button>
+          <button
+            onClick={() => {
+              alert("next");
+            }}
+            className="focus:outline-none"
+          >
+            <BsSkipEndCircleFill
+              size={30}
+              className="relative hover:scale-110 duration-300 focus:outline-none text-violet-600"
+            ></BsSkipEndCircleFill>
+          </button>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+const AudioPlayer = ({ src }) => {
+  const [
+    currentMusicId,
+    setCurrentMusicId,
+    Musics,
+    setMusics,
+    musicTime,
+    setMusicTime,
+  ] = useContext(MusicContext);
+  return (
+    <div className="isolate absolute block right-0 bottom-0 w-full rounded-tl-lg h-24 rounded-lg overflow-hidden">
+      <audio
+        src={Musics}
+        controls
+        className="absolute w-full h-full z-10"
+      ></audio>
+    </div>
+  );
+};
+
+// fin  partie musicbar
 function Body() {
   const [musics, setMusics] = useState([]);
   const [musicQuiJoue, setMusicQuiJoue] = useState("");
@@ -50,12 +204,12 @@ function Body() {
         <div className=" mb-4">
           <div
             style={{ display: error == null ? "none" : "block" }}
-            class="alert alert-error shadow-lg"
+            className="alert alert-error shadow-lg"
           >
             <div>
               <svg
                 xmlns="http://www.w3.org/2000/svg"
-                class="stroke-current flex-shrink-0 h-6 w-6"
+                className="stroke-current flex-shrink-0 h-6 w-6"
                 fill="none"
                 viewBox="0 0 24 24"
               >
@@ -72,11 +226,10 @@ function Body() {
           <StockListWithSearch data={musics} />
         </div>
         <div className=" mb-8">
-          <MusicBar />
+          <AudioPlayer />
         </div>
       </div>
     </MusicProvider>
   );
 }
-
 export default Body;
--- src/components/elements/boutonJouerMusique.js
+++ src/components/elements/boutonJouerMusique.js
@@ -5,24 +5,11 @@ import { MusicContext } from "./musicContext";
 
 function BouttonJouerMusique(props) {
   const [CasePlayStop, setSelectedIcon] = useState(1);
- // const [currentMusicId, setCurrentMusicId] = useState(MusicContext);
- const [currentMusicId, setCurrentMusicId,Musics,setMusics] = useContext(MusicContext);
- const [musicId, setMusicId] = useState(0);
+  // const [currentMusicId, setCurrentMusicId] = useState(MusicContext);
+  const [currentMusicId, setCurrentMusicId, Musics, setMusics] =
+    useContext(MusicContext);
+  const [musicId, setMusicId] = useState(0);
 
- const loadMusiqueChoisi = async () => {
-  await fetch("https://localhost:7246/Music/" + props.idMusique, {
-    mode: "cors",
-    method: "GET",
-  })
-    .then((response) => response.json())
-    .then((music) => {
-      setCurrentMusicId(music.id);
-      localStorage.setItem("idMusique", music.id);
-      setMusics(music.musicFile);
-    });
-};
-
- useEffect(() => {
   const loadMusiqueChoisi = async () => {
     await fetch("https://localhost:7246/Music/" + props.idMusique, {
       mode: "cors",
@@ -36,8 +23,22 @@ function BouttonJouerMusique(props) {
       });
   };
 
-  loadMusiqueChoisi();
-}, []);
+  useEffect(() => {
+    const loadMusiqueChoisi = async () => {
+      await fetch("https://localhost:7246/Music/" + props.idMusique, {
+        mode: "cors",
+        method: "GET",
+      })
+        .then((response) => response.json())
+        .then((music) => {
+          setCurrentMusicId(music.id);
+          localStorage.setItem("duration", music.duree);
+          setMusics(music.musicFile);
+        });
+    };
+
+    loadMusiqueChoisi();
+  }, []);
 
   function Play(e, idMusique) {
     e.preventDefault();
@@ -45,34 +46,28 @@ function BouttonJouerMusique(props) {
     loadMusiqueChoisi();
 
     setSelectedIcon(CasePlayStop === 1 ? 2 : 1);
-    
+
     console.log(audio);
-    
+
     //alert(props.idMusique);
     if (audio !== undefined) {
-    if (CasePlayStop === 1) {
-      audio.play();
-    } else if (CasePlayStop === 2) {
-      audio.pause();
+      if (CasePlayStop === 1) {
+        audio.play();
+      } else if (CasePlayStop === 2) {
+        audio.pause();
+      }
     }
   }
-  }
 
- 
   //console.log(Musics)
   return (
- 
     <button
       onLoad={loadMusiqueChoisi}
       onClick={(e) => Play(e, props.idMusique)}
       className="  bg-violet-500 hover:bg-violet-600 text-white font-bold py-2 px-4 rounded-full hover:scale-110 duration-300 transform-gpu transition ease-in-out delay-150"
     >
-       
       {CasePlayStop === 1 ? <ImPlay2 /> : <ImPause />}
-      <Music
-        idMusique={props.idMusique}
-        audioFile={Musics}
-      />
+      <Music idMusique={props.idMusique} audioFile={Musics} />
     </button>
   );
 }
