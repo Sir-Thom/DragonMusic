01871796e055ff2e727eb0635950ecf561fe7d9f
Thomas-Toulouse-Cegep <102597615+Thomas-Toulouse-Cegep@users.noreply.github.com>
2023-05-02 19:15:22 -0400

    Revert "Desktop app"

--- src/index.js
+++ src/index.js
@@ -1,57 +1,46 @@
-import { app, BrowserWindow } from 'electron';
+import React, { Fragment } from "react";
+import ReactDOM from "react-dom/client";
+import reportWebVitals from "./reportWebVitals";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+import { Inscription, Connexion } from "./pages/connexionInscription";
+import ErrorPage from "./pages/pageError";
+import Accueil from "./pages/pageAccueil";
+import AjoutMusique from "./pages/AjoutMusique";
 
-// Handle creating/removing shortcuts on Windows when installing/uninstalling.
-if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
-  app.quit();
-}
+/*
+note pour futur moi 
 
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
-let mainWindow;
+les class empêche d'utiliser les routes comme il faut
 
-const createWindow = () => {
-  // Create the browser window.
-  mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
-  });
+*/
+const router = createBrowserRouter([
+  {
+    path: "connexion",
+    element: <Connexion />,
+  },
+  {
+    path: "inscription",
+    element: <Inscription />,
+  },
+  {
+    path: "AjoutMusique",
+    element: <AjoutMusique />,
+  },
+  {
+    path: "/",
+    element: <Accueil />,
+    errorElement: <ErrorPage />,
+  },
+]);
 
-  // and load the index.html of the app.
-  mainWindow.loadURL(`file://${__dirname}/index.html`);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <Fragment>
+    <React.StrictMode>
+      <RouterProvider router={router} />
+    </React.StrictMode>
+  </Fragment>
+);
 
-  // Open the DevTools.
-  mainWindow.webContents.openDevTools();
-
-  // Emitted when the window is closed.
-  mainWindow.on('closed', () => {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    mainWindow = null;
-  });
-};
-
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.on('ready', createWindow);
-
-// Quit when all windows are closed.
-app.on('window-all-closed', () => {
-  // On OS X it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
-});
-
-app.on('activate', () => {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (mainWindow === null) {
-    createWindow();
-  }
-});
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and import them here.
+//reportWebVitals(console.log);
