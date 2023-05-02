cbc09aa650b3b13882cd97367c53e992fcb6b2b2
gabjacob04 <jedigabjacob@icloud.com>
2023-05-02 14:28:30 -0400

    bubble fait

--- src/pages/connexionInscription.js
+++ src/pages/connexionInscription.js
@@ -363,7 +363,7 @@ export function Inscription() {
             linkName="Se connecter"
             linkUrl="/connexion"
           />
-          <div style={{display : error == "" ? "none" : "block"}}>
+          <div style={{display : error === "" ? "none" : "block"}}>
             <div className="alert alert-error shadow-lg">
               <div>
                 <svg className="stroke-current flex-shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
