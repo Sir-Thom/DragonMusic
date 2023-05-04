8db15357d7e1a829c895ddafa560b455cb25b8a7
Sir-Thom <2134063@cegepba.qc.ca>
2023-05-03 20:32:12 -0400

    production settings api

--- src/components/body/body.js
+++ src/components/body/body.js
@@ -225,7 +225,7 @@ function Body() {
 
   const loadData = async () => {
     //exemple pour les changement de call api
-    fetch(process.env.REACT_APP_API_URL + "/Music", {
+    fetch("http://localhost:5000/Music", {
       mode: "cors",
 
       headers: {
