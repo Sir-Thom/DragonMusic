03fcd664d8fdf483891d58b53df1361a03523ee2
Sir-Thom <2134063@cegepba.qc.ca>
2023-05-03 21:26:55 -0400

    production settings api

--- API/API-AGT-Web/Program.cs
+++ API/API-AGT-Web/Program.cs
@@ -12,7 +12,6 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.Configure<KestrelServerOptions>(options =>
 {
-    options.AllowSynchronousIO = true;
 
     // default is 30 MB
     options.Limits.MaxRequestBodySize = int.MaxValue;
